88be09624a31fae0bc86533317765d3d149ccc38
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-01-08 10:37:09 -0300

    copy styles from older version

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import PomodoroApp from './PomodoroApp';
 
+import './scss/index.scss'
+
 ReactDOM.render(
   <React.StrictMode>
     <PomodoroApp />    
