343036c50b8bda1f65745f9d40147c0bab0655c4
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-01-09 20:58:30 -0300

    use context.currentTime in Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import zeroFill from '../helpers/zeroFill'
 
 const Timer = () => {
+  const context = useContext(GlobalContext)
+
   return (
-    <p className="pomodoro__timer mt-5 mb-2 ta--center fw--600">25:00</p>
+    <p className="pomodoro__timer mt-5 mb-2 ta--center fw--600">
+      {Math.floor(context.timerState.currentTime / 60)}:
+      {zeroFill(context.timerState.currentTime % 60)}
+    </p>
   )
 }
 
---
+++ src/helpers/zeroFill.js
@@ -0,0 +1,3 @@
+const zeroFill = (num) => `${num < 10 ? '0' : ''}${num}`;
+
+export default zeroFill
