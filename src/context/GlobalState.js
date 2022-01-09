3c41af81be1f62f15d2d5325d2eef494b556c44c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2022-01-09 20:58:02 -0300

    add actions in GlobalState

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -14,12 +14,13 @@ const initialState = {
     intervals: 4,
   },
   timerState: {
-    currentTime: 25 * 60,
+    currentTime: null,
     isRunning: false,
     isBreakTime: false,
     currentInterval: 1,
   },
 }
+initialState.timerState.currentTime = initialState.settings.pomodoroTime * 60
 
 const GlobalContext = createContext(initialState)
 
@@ -30,11 +31,122 @@ const GlobalProvider = ({ children }) => {
     localStorage.setItem('pomodoro-app', JSON.stringify(state))
   }, [state])
 
+  const setPomodoroTime = (newTime) => {
+    if (!state.timerState.isBreakTime) {
+      const remainingTime =
+        newTime -
+        (state.settings.pomodoroTime * 60 - state.timerState.currentTime)
+
+      dispatch({
+        type: 'UPDATE_TIMER_STATE',
+        payload: {
+          key: 'currentTime',
+          value: remainingTime,
+        },
+      })
+    }
+
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'pomodoroTime',
+        value: newTime,
+      },
+    })
+  }
+
+  const setShortBreakTime = (newTime) => {
+    if (state.timerState.isBreakTime) {
+      const remainingTime =
+        newTime -
+        (state.settings.shortBreakTime * 60 - state.timerState.currentTime)
+
+      dispatch({
+        type: 'UPDATE_TIMER_STATE',
+        payload: {
+          key: 'currentTime',
+          value: remainingTime,
+        },
+      })
+    }
+
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'shortBreakTime',
+        value: newTime,
+      },
+    })
+  }
+
+  const setLongBreakTime = (newTime) => {
+    if (
+      state.timerState.isBreakTime &&
+      state.timerState.currentInterval === state.settings.intervals
+    ) {
+      const remainingTime =
+        newTime -
+        (state.settings.longBreakTime * 60 - state.timerState.currentTime)
+
+      dispatch({
+        type: 'UPDATE_TIMER_STATE',
+        payload: {
+          key: 'currentTime',
+          value: remainingTime,
+        },
+      })
+    }
+
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'longBreakTime',
+        value: newTime,
+      },
+    })
+  }
+
+  const toggleAutoStartPomodoros = () => {
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'autoStartBreaks',
+        value: !state.settings.autoStartBreaks,
+      },
+    })
+  }
+
+  const toggleAutoStartBreaks = () => {
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'autoStartPomodoros',
+        value: !state.settings.autoStartPomodoros,
+      },
+    })
+  }
+
+  const setIntervals = (newIntervals) => {
+    dispatch({
+      type: 'UPDATE_SETTINGS',
+      payload: {
+        key: 'intervals',
+        value: newIntervals,
+      },
+    })
+  }
+
   return (
     <GlobalContext.Provider
       value={{
         settings: state.settings,
         timerState: state.timerState,
+        setPomodoroTime,
+        setShortBreakTime,
+        setLongBreakTime,
+        toggleAutoStartPomodoros,
+        toggleAutoStartBreaks,
+        setIntervals,
       }}
     >
       {children}
